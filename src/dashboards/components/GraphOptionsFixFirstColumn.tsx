795cf466c797ef2f349b60ee65dc5f231db24fbf
Joel Fried <jfried@influxdata.com>
2021-01-29 16:56:19 -0500

    chore: refactor GraphOptionsFixFirstColumn test and SelectCollectorsStep test and use imported instructions in PluginConfigSwitcher test

--- src/dashboards/components/GraphOptionsFixFirstColumn.tsx
+++ src/dashboards/components/GraphOptionsFixFirstColumn.tsx
@@ -9,11 +9,15 @@ const GraphOptionsFixFirstColumn: SFC<Props> = ({
   fixed,
   onToggleFixFirstColumn,
 }) => (
-  <div className="form-group col-xs-12">
+  <div
+    className="form-group col-xs-12"
+    data-testid="graph-options-first-column"
+  >
     <div className="form-control-static">
       <input
         type="checkbox"
         id="fixFirstColumnCheckbox"
+        data-testid="fix-first-column-checkbox"
         checked={!!fixed}
         onChange={onToggleFixFirstColumn}
       />
